97e39cb47545671f907d32f5ee27bb482ae4575a
Hchlasmuck <axel.ahlbeck@gmail.com>
2015-09-23 13:06:51 +0200

    simple percentage overlapping in tests/overlap_test.c

--- software/tests/overlap_test.c
+++ software/tests/overlap_test.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include "../constants.h"
 
-int check_overlap(char* wall, char* bin_img){
+int check_overlap(unsigned char* wall, unsigned char* bin_img){
 	
 	/*
 	*	Check for overlap between bin_img and wall
 	*/
 	int i;
 	for (i = 0; i < BINARY_IMAGE_SIZE; i++) {
-	    if (*wall & *bin_img) return 1;
-	    wall++;
-	    bin_img++;
+	    if (wall[i] & bin_img[i]) return 1;
 	}	
 	return 0;
 	/*
@@ -18,19 +16,34 @@ int check_overlap(char* wall, char* bin_img){
 	*/
 }
 
+int percentage_overlap(unsigned char* wall, unsigned char* bin_img)
+{
+    int pixels_overlapping = 0;
+    int i;
+    unsigned int bit;
+    for (i = 0; i < BINARY_IMAGE_SIZE; i++) {
+	for (bit = 1; bit <= 128; bit = bit << 1) {
+	    if (wall[i] & bin_img[i] & bit) pixels_overlapping++;
+	}
+    }
+    double q = (double)pixels_overlapping / ((double)(BINARY_IMAGE_SIZE) * 8.0);
+    q *= 100;
+    return (int)q;
+}
+
 int main(int argc, char *argv[])
 {
-    char wall1[WALL_SIZE];
-    char bin_img1[BINARY_IMAGE_SIZE];
-    
+    unsigned char wall1[WALL_SIZE];
+    unsigned char bin_img1[BINARY_IMAGE_SIZE];
+
     int i;
     for (i = 0; i < BINARY_IMAGE_SIZE; i++) {
 	wall1[i] = 255;
-	bin_img1[i] = 0;
+	bin_img1[i] = 3;
     }
     //bin_img1[480] = 1;
 
-    int ok = check_overlap(wall1, bin_img1);
+    int ok = percentage_overlap(wall1, bin_img1);
     printf("overlap: %d\n", ok);
 }
 
