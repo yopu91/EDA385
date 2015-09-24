1b3805c9485775233d146588c090537ce8da3faf
Erik Nimmermark <fpr05eni@student.lu.se>
2015-09-24 11:04:39 +0200

    game_logic changes ... not sure what

--- software/game_logic.c
+++ software/game_logic.c
@@ -37,7 +37,7 @@ int Status;
 			XTC_AUTO_RELOAD_OPTION);
 
 	Value1 = XTmrCtr_GetValue(TmrCtrInstancePtr, TIMER_COUNTER_0);
-	
+
 	XTmrCtr_Start(TmrCtrInstancePtr, TIMER_COUNTER_0);
 	//--- divide here
 	Value2 = XTmrCtr_GetValue(TmrCtrInstancePtr, TIMER_COUNTER_0);
@@ -52,7 +52,7 @@ int Status;
 
 
 /*
-*	Wall data strucure: Draw until 1st column, start again at 2nd column and draw until 3rd column ... 
+*	Wall data strucure: Draw until 1st column, start again at 2nd column and draw until 3rd column ...
 *	one wall = 8 * 6 * 480 / SCALING_FACTOR bits
 *	---------------- 255,	255,	255,	255,	255,	255
 *	---------------- 255,	255,	255,	255,	255,	255
@@ -63,7 +63,7 @@ int Status;
 *
 *	With this scaling factor the size of the walls will be 2304 bits.
 *	This is probably only worth it if can afford to and want to go with higher resolution wall images,
-* 	otherwise we might as well go with a 64*48 bit array which would result in 3072 bit images, 
+* 	otherwise we might as well go with a 64*48 bit array which would result in 3072 bit images,
 *	if we dont use the entire width of the screen for walls we'll save even less memory.
 */
 
@@ -73,34 +73,34 @@ int main(void)
 {
 	/*
 	*	Protocol for sending data to HW
-	*	bit#	0-23 Necessary padding?			0-1					2				3				4-7 
+	*	bit#	0-23 Necessary padding?			0-1					2				3						4-7 
 	*									"Special Image"		Wall Available		Pass/Fail		Timer to display
 	*	If the Wall Available bit it set, the protocol message is followed by the wall image
 	*/
-	
+
 	/*
 	*	"Special images"
 	*	11000000	-	Startscreen
 	*/
-	
-	
+
+
 	char protocol = 192; // 11000000
 	putfsl(protocol, 0);
-	
+
 	char* wall_sets = init_walls();
-	
+
 	char bin_img[BINARY_IMAGE_SIZE];
 
 	time_t t;
 	int curr_wall;
 	bool fail;
-	
+
 	clock_t end_timer;
 	clock_t time_now;
-	
+
 	//what seed to use?
 	srand((unsigned) time(&t));
-	
+
 	while(1){
 		/*
 		* Create a 2 second delay between walls
@@ -109,16 +109,16 @@ int main(void)
 		XTmrCtr_Start(TmrCtrInstancePtr, TIMER_COUNTER_0);
 		end_timer = XTmrCtr_GetValue(TmrCtrInstancePtr, TIMER_COUNTER_0) + 200000000;
 		XTmrCtr_SetOptions(TmrCtrInstancePtr, TIMER_COUNTER_0, 0);
-		
+
 		//busy wait, how to fix?
 		while(time_now < end_timer){
 			time_now = XTmrCtr_GetValue(TmrCtrInstancePtr, TIMER_COUNTER_0);
 		}
-		
-		
-		
+
+
+
 		curr_wall_index = rand() % WALLCOUNT;
-		
+
 		/*
 		*	send wall image to hardware
 		*/
@@ -128,15 +128,15 @@ int main(void)
 		for(int i = 0; i<WALL_SIZE/4, i++){
 			putfsl(curr_wall+i, 0);
 		}
-		
-		
+
+
 		// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-		/*	
+		/*
 		*	Wait x seconds before checking for overlap. Display a countdown timer every second?
 		*/
 		// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-		
-		
+
+
 		// ---------------------------------------------------------------
 		/*
 		* 	Alternative approach, check for overlaps during "downtime" to have real time feedback on
@@ -145,60 +145,59 @@ int main(void)
 		/* Timer for non-board
 		time_now = clock();
 		*/
-		
+
 		/* Timer for board
 		*/
 		time_now = XTmrCtr_GetValue(TmrCtrInstancePtr, TIMER_COUNTER_0);
 		XTmrCtr_Start(TmrCtrInstancePtr, TIMER_COUNTER_0);
-		
+
 		/*
-		*	The exact timings might have to be adjusted depending on how long it takes to generate the 
-		*	binary image	
+		*	The exact timings might have to be adjusted depending on how long it takes to generate the
+		*	binary image
 		*/
 		/*	Timer for non-board
-		end_timer = clock() + 300000000; // 3 sec delay with 100Mhz clock, 
+		end_timer = clock() + 300000000; // 3 sec delay with 100Mhz clock,
 		*/
 		/*	Timer for board
 		*/
 		end_timer = XTmrCtr_GetValue(TmrCtrInstancePtr, TIMER_COUNTER_0) + 300000000;
 		while(time_now < end_timer){
-			unsigned char display_time = ((end_timer - time_now) / 100000000) +1; // countdown timer to be displayed
-			
+			unsigned char display_time = ((end_timer - time_now) / 100000000) + 1; // countdown timer to be displayed
+
 			bool fail = check_overlap(&wall_sets[curr_wall*WALL_SIZE], bin_img);
-			
-			
+
+
 		/*
 		*	Send fail and display_time to HW to indicate passing or failing state and time remaining.
 		*/
 		/*	Timer for non-board
 			time_now = clock(); // updating the timer
 		*/
-		
+
 		/*	Timer for board
 		*/
 			time_now = XTmrCtr_GetValue(TmrCtrInstancePtr, TIMER_COUNTER_0);
 			XTmrCtr_Start(TmrCtrInstancePtr, TIMER_COUNTER_0);
 			XTmrCtr_SetOptions(TmrCtrInstancePtr, TIMER_COUNTER_0, 0);
-			
+
 		}
 		// ---------------------------------------------------------------
-		
+
 		bool fail = check_overlap(&wall_sets[curr_wall*WALL_SIZE], bin_img);
-		
+
 		/*
 		*		send result and timer(0) to HW (display "FAIL!" || "PASS!" image stored in hw?)
 		*		probably add a delay here as well.
 		*/
-		
+
 	}
-	
-	
+
+
 }
 
 char* init_walls(void){
 	char* walls = calloc(WALLCOUNT*WALL_SIZE, sizeof(char));
-	
+
 	// Create walls
 	return walls;
 }
-
